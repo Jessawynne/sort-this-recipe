785053d8fcb028fe43d135945f1b4215f2ff5b63
Jessawynne <jessawynne@gmail.com>
2016-03-16 09:25:47 -0500

    heroku connected

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/', (req, res) => {
-  res.send('Server Running');
+  res.render('index');
 });
 
 mongoose.connect('mongodb://localhost:27017/sort-this-recipe', (err) => {
   if (err) throw err;
 
   app.listen(port, () => {
-    console.log(`Sort This Recipe server running on port: ${PORT}`);
+    console.log(`Sort This Recipe server running on port: ${port}`);
   });
 });
+
